9ded0c37a7c4157da02eeade772c9b1cafc9065c
TheKazuto <watarizangado@gmail.com>
2026-03-02 14:25:39 -0300

    Add files via upload

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -35,7 +35,7 @@ const SocialLinks = [
   },
   {
     name: 'Docs',
-    href: 'https://docs.monad.xyz',
+    href: 'https://shinkalabs.gitbook.io/dashboard',
     icon: (
       <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
         <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/>
